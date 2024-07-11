7ce96ce806d66fc56bc65319cc8de300f304aac9
blaineconnor <fesarman@anadolu.edu.tr>
2024-07-11 15:11:28 +0300

    .

--- ImageTextureMaterial/ContentView.swift
+++ ImageTextureMaterial/ContentView.swift
@@ -7,10 +7,73 @@
 
 import SwiftUI
 import RealityKit
+import Combine
 
 struct ContentView : View {
     var body: some View {
         ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+class Coordinator {
+    var arView: ARView?
+    var cancellable: Cancellable?
+    
+    func setup(){
+        
+        guard let arView = arView else {
+            return
+        }
+        
+        let anchor = AnchorEntity(plane: .horizontal)
+        
+        let mesh = MeshResource.generateBox(width: 0.3, height: 0.3, depth: 0.3, cornerRadius: 0, splitFaces: true)
+        let box = ModelEntity(mesh: mesh)
+        
+        cancellable = TextureResource.loadAsync(named: "lola")
+            .append(TextureResource.loadAsync(named: "purple_flower"))
+            .append(TextureResource.loadAsync(named: "cover.jpg"))
+            .append(TextureResource.loadAsync(named: "DSC_0003.JPG"))
+            .append(TextureResource.loadAsync(named: "DSC_0117.JPG"))
+            .append(TextureResource.loadAsync(named: "DSC_0171.JPG"))
+            .collect().sink(receiveCompletion: { [weak self] completion in
+                if case let .failure(error) = completion {
+                    fatalError("Unable to load texture \(error)")
+                }
+                self?.cancellable?.cancel()
+                
+            }, receiveValue: { textures in
+                var materials: [UnlitMaterial] = []
+                
+                textures.forEach { texture in
+                    var material = UnlitMaterial()
+                    material.color = .init(tint: .white, texture: .init(texture))
+                    materials.append(material)
+                }
+                
+                box.model?.materials = materials
+                anchor.addChild(box)
+                arView.scene.addAnchor(anchor)
+            }
+            )
+        
+        
+        
+        
+        
+        //        let mesh = MeshResource.generateBox(size: 0.3)
+        //        let box = ModelEntity(mesh: mesh)
+        //
+        //        let texture = try? TextureResource.load(named: "purple_flower")
+        //
+        //        if let texture = texture {
+        //            var material = UnlitMaterial()
+        //            material.color = .init(tint: .white, texture: .init(texture))
+        //            box.model?.materials = [material]
+        //        }
+        //
+        //        anchor.addChild(box)
+        //        arView.scene.addAnchor(anchor)
     }
 }
 
@@ -19,28 +82,18 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
-
-        // Create a cube model
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-        let model = ModelEntity(mesh: mesh, materials: [material])
-        model.transform.translation.y = 0.05
-
-        // Create horizontal plane anchor for the content
-        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
-
-        // Add the horizontal plane anchor to the scene
-        arView.scene.anchors.append(anchor)
-
+        
+        context.coordinator.arView = arView
+        context.coordinator.setup()
+        
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
 }
 
-#Preview {
-    ContentView()
-}
